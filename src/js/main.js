0a52ba62376464cf873bab2ccad407882b19961a
Yurii <legostaevyura999@gmail.com>
2023-07-14 02:36:58 +0300

    feat(index.pug): add 2 section + mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-	console.log("some text");
+	mobileMenu();
+
+	function mobileMenu() {
+		const switcher = document.querySelector(".menu__switcher");
+		const menu = document.querySelector("nav.navigation");
+		let mobMenuActive = false;
+
+		switcher.addEventListener("click", () => {
+			if (mobMenuActive) {
+				switcher.classList.remove("menu__switcher_active");
+				menu.style.left = "-30rem";
+				mobMenuActive = false;
+				document.body.style.overflow = "";
+			} else {
+				switcher.classList.add("menu__switcher_active");
+				menu.style.left = 0;
+				mobMenuActive = true;
+				document.body.style.overflow = "hidden";
+			}
+		});
+	}
 });
